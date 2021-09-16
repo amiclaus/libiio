a72e1bf5e991c7a184e9a6e291aa82540af51011
Raluca Chis <raluca.chis@analog.com>
2021-09-16 13:59:27 +0300

    Deploy autogenerated docs for libiio v0.23-g21b466d
    Signed-off-by: Autogenerated by Azure Pipelines <cse-ci-notifications@analog.com>

--- v0.23/python/_static/documentation_options.js
+++ v0.23/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.23.gb814d31',
+    VERSION: '0.23.g21b466d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
